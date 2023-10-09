f989cd77cf2a43d0b9a6dc6ad826a32fbd8f57dc
NotaTree1 <rowankiksen@gmail.com>
2023-10-09 14:20:18 +0200

    changed to be visible on user end.
    Signed-off-by: NotaTree1 <rowankiksen@gmail.com>

--- data_access/FilmSave.cs
+++ data_access/FilmSave.cs
@@ -28,6 +28,30 @@ class FilmSave
         else return new List<Film> {};
     }
 
+    public void ReadJsonFile()
+    {
+        if (File.Exists(this.PathName))
+        {
+            StreamReader reader = new(this.PathName);
+            string filefromjson = reader.ReadToEnd();
+            List<Film> films = JsonConvert.DeserializeObject<List<Film>>(filefromjson)!;
+            reader.Close();
+            foreach (Film film in films)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Movie Information:");
+                Console.WriteLine($"Title: {film.Title}");
+                Console.WriteLine($"Genres: {string.Join(", ", film.Genres!)}");
+                Console.WriteLine($"§Director: {film.Director}");
+                Console.WriteLine($"Release Year: {film.ReleaseYear}");
+                Console.WriteLine($"Film RunTime: {film.FilmRunTime} minutes");
+                Console.WriteLine($"Film Price: Euro {film.FilmPrice}");
+                Console.WriteLine($"Film Rating: {film.FilmRating}");
+                Console.WriteLine();
+            }
+        }
+    }
+
     public void AddToJson(Film filmToAdd)
     {
         List<Film> films = this.ReadFilms();
