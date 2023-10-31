c9ca04241888e7ee2720e3b90bbcb57c4d0e8695
jvp010 <121518493+jvp010@users.noreply.github.com>
2023-10-31 13:47:31 +0100

    mijn code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+<<<<<<< Updated upstream
 ﻿using data_acces.Tests;
+=======
+﻿// using data_acces.Tests;
+>>>>>>> Stashed changes
 class Program
 {
     static void Main(string[] args)
     {
+<<<<<<< Updated upstream
         foreach (string arg in args)
         {
             Console.WriteLine(arg);
@@ -28,5 +33,46 @@ class Program
             }
         }
         FrontPage.MainMenu();
+=======
+
+        List<Customer> customers = Customer.LoadFromJsonFile();
+        Film myFilm = new Film("Sample Film", 120, 12.99, 4.5, new List<string> { "Action", "Adventure" }, "John Doe", new List<string>());
+        foreach (Customer customer in customers)
+        {
+            FilmSave.AddCustomerToFilm(myFilm, customer);
+        }
+
+
+
+
+
+
+
+        // foreach (string arg in args)
+        // {
+        //     Console.WriteLine(arg);
+        //     switch(arg)
+        //     {
+        //         case "TESTSAVE":
+        //         {
+        //             FilmSaveTest saveTest = new();
+        //             saveTest.read_films_test("Test film", 1, 1, 1);
+        //             break;
+        //         }
+        //         case "TESTMAP":
+        //         {
+        //             AuditoriumMap500 map500 = new AuditoriumMap500();
+        //             map500.TakeSeats();
+        //             return;
+        //         }
+        //         default:
+        //         {
+        //             Console.WriteLine("this is not a valid one");
+        //             break;
+        //         }
+        //     }
+        // }
+        // FrontPage.MainMenu();
+>>>>>>> Stashed changes
     }
 }
