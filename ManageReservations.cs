96da8261896ad757725c71523a9879a35aafc61c
JestinJayden <119542531+JestinJayden@users.noreply.github.com>
2023-11-13 09:36:36 +0100

    Add files via upload

---
+++ ManageReservations.cs
@@ -0,0 +1,179 @@
+using Newtonsoft.Json;
+
+public class ManageReservations
+{
+    public static void ReservationsOptions()
+    {
+        Console.Clear();
+        List<string> options = new List<string>()
+        {
+            "Reservatie(s) toevoegen",
+            "Reservatie(s) verwijderen"
+        };
+        int selectedIndex = 0;
+        ConsoleKeyInfo keyInfo;
+        do
+        {
+            Console.Clear();
+
+            for (int i = 0; i < options.Count; i++)
+            {
+                if (i == selectedIndex)
+                {
+                    Console.WriteLine("--> " + options[i]);
+                }
+                else
+                {
+                    Console.WriteLine("    " + options[i]);
+                }
+            }
+            keyInfo = Console.ReadKey();
+
+            if (keyInfo.Key == ConsoleKey.W && selectedIndex > 0 || keyInfo.Key == ConsoleKey.UpArrow && selectedIndex > 0)
+            {
+                selectedIndex--;
+            }
+            else if (keyInfo.Key == ConsoleKey.S && selectedIndex < options.Count - 1 || keyInfo.Key == ConsoleKey.DownArrow && selectedIndex < options.Count - 1)
+            {
+                selectedIndex++;
+            }
+
+        } while (keyInfo.Key != ConsoleKey.Enter & keyInfo.Key != ConsoleKey.Escape);
+
+        if (options[selectedIndex] == "Reservatie(s) toevoegen")
+        {
+            Console.Clear();
+            MovieTitleToManageReservations(true);
+
+        }
+        if (options[selectedIndex] == "Reservatie(s) verwijderen")
+        {
+            Console.Clear();
+            MovieTitleToManageReservations(false);
+
+        }
+
+    }
+
+    private static void MovieTitleToManageReservations(bool IsAdd)
+    {
+        System.Console.WriteLine("Voor welke film wil je een reservatie toevoegen?:\n");
+        string movieTitle = Console.ReadLine();
+        List<MovieScheduleInformation> allMovies = ReadDataFromJson();
+        foreach (var movie in allMovies)
+        {
+            if (movie.Title == movieTitle)
+            {
+                if (IsAdd)
+                    AddReservation(movieTitle);
+                else
+                    RemoveReservation(movieTitle);
+            }
+            else
+            {
+                System.Console.WriteLine("Sorry maar die film bestaat niet in de database. \nZorg ervoor dat je de titel goed spelt.");
+                MovieTitleToManageReservations(IsAdd);
+            }
+        }
+    }
+
+    private static void AddReservation(string MovieTitle)
+    {
+        List<MovieScheduleInformation> AllMoviesFromJson = ReadDataFromJson();
+        foreach (var movie in AllMoviesFromJson)
+        {
+            if (movie.Title == MovieTitle)
+            {
+                List<List<string>> movieAuditorium = movie.ScreeningTimeAndAuditorium["11-11-2023"];
+                switch (movieAuditorium.Count)
+                {
+                    case 14:
+                        AuditoriumMap150 map150 = new AuditoriumMap150();
+                        map150.TakeSeats(MovieTitle, false);
+                        break;
+                    case 19:
+                        AuditoriumMap300 map300 = new AuditoriumMap300();
+                        map300.TakeSeats(MovieTitle, false);
+                        break;
+                    case 20:
+                        AuditoriumMap500 map500 = new AuditoriumMap500();
+                        map500.TakeSeats(MovieTitle, false);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+
+    private static void RemoveReservation(string MovieTitle)
+    {
+        System.Console.WriteLine("Welke reservatie wil je verwijderen?:\n");
+        string ConfirmationCode = Console.ReadLine();
+        List<MovieScheduleInformation> AllMoviesFromJson = ReadDataFromJson();
+        foreach (var movie in AllMoviesFromJson)
+        {
+            if (movie.Title == MovieTitle)
+            {
+                foreach (string reservation in movie.ReservationsList)
+                {
+                    if (reservation.Contains(ConfirmationCode))
+                    {
+                        movie.ReservationsList.Remove(reservation);
+                        switch (movie.ScreeningTimeAndAuditorium["11-11-2023"].Count)
+                        {
+                            case 14:
+                                AuditoriumMap150 map150 = new AuditoriumMap150();
+                                map150.TakeSeats(MovieTitle, true);
+                                break;
+                            case 19:
+                                AuditoriumMap300 map300 = new AuditoriumMap300();
+                                map300.TakeSeats(MovieTitle, true);
+                                break;
+                            case 20:
+                                AuditoriumMap500 map500 = new AuditoriumMap500();
+                                map500.TakeSeats(MovieTitle, true);
+                                break;
+                            default:
+                                break;
+                        }
+                        System.Console.WriteLine("Reservatie is verwijderd");
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Sorry maar die reservatie bestaat niet in de database. \nZorg ervoor dat je de reservatie code goed spelt.");
+                        RemoveReservation(MovieTitle);
+                    }
+                }
+            }
+
+        }
+    }
+
+
+        private static List<MovieScheduleInformation>? ReadDataFromJson()
+        {
+            if (File.Exists("MovieScheduleInformation.json"))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader("MovieScheduleInformation.json"))
+                    {
+                        string json = reader.ReadToEnd();
+                        List<MovieScheduleInformation> ExistingData = JsonConvert.DeserializeObject<List<MovieScheduleInformation>>(json)!;
+                        if (ExistingData == null)
+                        {
+                            ExistingData = new List<MovieScheduleInformation>();
+                        }
+                        return ExistingData;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error reading JSON data: {ex.Message}");
+                }
+            }
+            return null;
+        }
+    }
---
+++ MovieScheduleInformation.cs
@@ -0,0 +1,97 @@
+using Newtonsoft.Json;
+
+public class MovieScheduleInformation
+{
+    public string? Title { get; set; }
+    public Dictionary<string, List<List<string>>> ScreeningTimeAndAuditorium { get; set; } = new Dictionary<string, List<List<string>>>();
+    public List<string> ReservationsList { get; set; } = new List<string>();
+
+    public void AddTitleAndScreeningTimeAndAuditorium(string AddTitle, string Date, List<List<string>> Auditorium, string ConfirmationCode)
+    {
+        ScreeningTimeAndAuditorium.Add(Date, Auditorium);
+        List<MovieScheduleInformation> AddToJson = new List<MovieScheduleInformation>
+        {
+            new MovieScheduleInformation
+            {
+                Title = AddTitle,
+                ScreeningTimeAndAuditorium = new Dictionary<string, List<List<string>>>(ScreeningTimeAndAuditorium),
+                ReservationsList = new List<string>(ReservationsList)
+            }
+        };
+
+        WriteDataFromJsin(AddToJson, ConfirmationCode);
+    }
+
+    private void WriteDataFromJsin(List<MovieScheduleInformation> AddToJson, string ConfirmationCode)
+    {
+        List<MovieScheduleInformation> ExistingData = ReadDataFromJson()!;
+        if (File.Exists("MovieScheduleInformation.json"))
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("MovieScheduleInformation.json"))
+                {
+                    foreach (MovieScheduleInformation movie in ExistingData)
+                    {
+                        if (movie.Title == AddToJson[0].Title)
+                        {
+                            movie.ScreeningTimeAndAuditorium["11-11-2023"] = AddToJson[0].ScreeningTimeAndAuditorium["11-11-2023"];
+                            movie.ReservationsList.Add(ConfirmationCode);
+
+                            string List3Json = JsonConvert.SerializeObject(ExistingData, Formatting.Indented);
+                            writer.Write(List3Json);
+                            return;
+                        }
+                    }
+
+
+
+                    foreach (MovieScheduleInformation movieSchedule in AddToJson)
+                    {
+                        movieSchedule.ReservationsList.Add(ConfirmationCode);
+                        ExistingData.Add(movieSchedule);
+                        
+                    }
+                    string List2Json = JsonConvert.SerializeObject(ExistingData, Formatting.Indented);
+                    writer.Write(List2Json);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading JSON data: {ex.Message}");
+            }
+        }
+    }
+
+    private List<MovieScheduleInformation>? ReadDataFromJson()
+    {
+        if (File.Exists("MovieScheduleInformation.json"))
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader("MovieScheduleInformation.json"))
+                {
+                    string json = reader.ReadToEnd();
+                    List<MovieScheduleInformation> ExistingData = JsonConvert.DeserializeObject<List<MovieScheduleInformation>>(json)!;
+                    if (ExistingData == null)
+                    {
+                        ExistingData = new List<MovieScheduleInformation>();
+                    }
+                    return ExistingData;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading JSON data: {ex.Message}");
+            }
+        }
+        return null;
+    }
+
+
+    public void AddReservation(string Confirmationcode)
+    {
+        if (Confirmationcode.Contains(':'))
+            ReservationsList.Add(Confirmationcode);
+    }
+}
