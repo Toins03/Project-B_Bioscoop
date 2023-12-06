d2e05b2cb4b0435c3aa75089415dc3b5c5474619
jvp010 <121518493+jvp010@users.noreply.github.com>
2023-12-06 14:00:58 +0100

    Add files via upload

---
+++ ShoppingCartTest.cs
@@ -0,0 +1,57 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace shopping_cart.Tests;
+
+
+[TestClass]
+public class ShoppingCartTests
+{
+    [TestInitialize]
+    [TestMethod]
+    public void AddToShoppingCart_ShouldIncreaseItemCount()
+    {
+        // Arrange
+        ShoppingCart cart = new ShoppingCart();
+        Snack snack = new Snack("Test Snack", 2.99); // Assuming you have a Snack class
+
+        // Act
+        cart.AddtoShoppingCart(snack);
+
+        // Assert
+        Assert.AreEqual(1, cart.shoppingcart.Count);
+        Assert.AreEqual(snack, cart.shoppingcart[0]);
+    }
+
+    [TestMethod]
+    public void ShoppingCartCosts_ShouldCalculateTotalCost()
+    {
+        // Arrange
+        ShoppingCart cart = new ShoppingCart();
+        Snack snack1 = new Snack("Snack 1", 2.99);
+        Snack snack2 = new Snack("Snack 2", 1.99);
+
+        // Act
+        cart.AddtoShoppingCart(snack1);
+        cart.AddtoShoppingCart(snack2);
+
+        // Assert
+        Assert.AreEqual(4.98, cart.ShoppingCartCosts());
+    }
+
+    [TestMethod]
+    public void ModifyShoppingCart_ShouldRemoveSelectedItems()
+    {
+        // Arrange
+        ShoppingCart cart = new ShoppingCart();
+        Snack snack1 = new Snack("Snack 1", 2.99);
+        Snack snack2 = new Snack("Snack 2", 1.99);
+
+        cart.AddtoShoppingCart(snack1);
+        cart.AddtoShoppingCart(snack2);
+
+        // Act
+        cart.ModifyShoppingCartForTests("Snack 1", 1);
+
+        // Assert
+        Assert.AreEqual(1, cart.shoppingcart.Count); // Assuming all items were removed
+    }
+}
