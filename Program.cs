95662ca57cb12a69f89712afffe2bb7475059999
JestinJayden <119542531+JestinJayden@users.noreply.github.com>
2023-12-08 13:03:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// using data_acces.Tests;
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (string arg in args)
+        {
+
+            Console.WriteLine(arg);
+            switch (arg)
+            {
+                case "snack":
+                    {
+                        Snack.ChooseToAddSnackOrNot();
+                        break;
+                    }                
+                default:
+                    {
+                        Console.WriteLine("this is not a valid one");
+                        break;
+                    }
+            }
+        }
+        Snack.ChooseToAddSnackOrNot();
+    }
+}
