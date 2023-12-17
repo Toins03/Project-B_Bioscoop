48b6c1d056521422aad399bd3ef46e37122da2c8
NotaTree1 <rowankiksen@gmail.com>
2023-12-17 14:13:08 +0100

    nu kan je zien welke films je hebt gereserveerd

--- LogIn/ViewCustomerInfo.cs
+++ LogIn/ViewCustomerInfo.cs
@@ -13,6 +13,16 @@ class ViewCustomerInfo
             Console.WriteLine($"Gebruikersnaam: {toView.UserName}");
             Console.WriteLine($"Email account: {toView.Email}");
             Console.WriteLine($"Wachtwoord: {toView.Password}");
+            if (toView.RentedMovieInfo is null);
+            else if (toView.RentedMovieInfo.Count >= 1)
+            {
+                foreach (RentedMovieInfo info in toView.RentedMovieInfo)
+                {
+                    Console.WriteLine($"Film gereserveerd {info.FilmTitle}");
+                    Console.WriteLine($"stoelen gereserveerd voor deze film {info.SeatsTaken}");
+                    Console.WriteLine($"Tijd van deze film gereserveerd {info.TimeViewing}");
+                }
+            }
             Console.WriteLine("\ndruk op een willekeurige knop om terug te gaan");
             Console.ReadKey();
         }
