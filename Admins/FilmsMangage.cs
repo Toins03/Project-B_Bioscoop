9020a5b986bd63324477ad2c057de89a71c61498
NotaTree1 <rowankiksen@gmail.com>
2024-01-10 15:17:30 +0100

    Nu kan je weer een film toevoegen

--- Admins/FilmsMangage.cs
+++ Admins/FilmsMangage.cs
@@ -48,87 +48,87 @@ static class FilmsManage
     //     FilmmanageMenu();
     // }
 
-    private static void AddFilm()
-    {
-        System.Console.WriteLine("Please input the name of the Film. To go back to the Film manager keep this line empty.");
-        string ToAddName = Console.ReadLine()!;
-        if (ToAddName is null) return;
-        else if (ToAddName == "") return;
-        Console.Clear();
+    // private static void AddFilm()
+    // {
+    //     System.Console.WriteLine("Please input the name of the Film. To go back to the Film manager keep this line empty.");
+    //     string ToAddName = Console.ReadLine()!;
+    //     if (ToAddName is null) return;
+    //     else if (ToAddName == "") return;
+    //     Console.Clear();
 
-        int ToAddRunTime;
-        while (true)
-        {
-            System.Console.WriteLine("Please input the runtime of the Film in minutes. To go back to the Film manager keep this line empty.");
-            string ToAddRunTimestring = Console.ReadLine()!;
-            if (ToAddRunTimestring is null) return;
-            else if (ToAddRunTimestring == "") return;
-            if (int.TryParse(ToAddRunTimestring, out _))
-            {
-                ToAddRunTime = Convert.ToInt32(ToAddRunTimestring);
-                break;
-            }
-        }
-        Console.Clear();
+    //     int ToAddRunTime;
+    //     while (true)
+    //     {
+    //         System.Console.WriteLine("Please input the runtime of the Film in minutes. To go back to the Film manager keep this line empty.");
+    //         string ToAddRunTimestring = Console.ReadLine()!;
+    //         if (ToAddRunTimestring is null) return;
+    //         else if (ToAddRunTimestring == "") return;
+    //         if (int.TryParse(ToAddRunTimestring, out _))
+    //         {
+    //             ToAddRunTime = Convert.ToInt32(ToAddRunTimestring);
+    //             break;
+    //         }
+    //     }
+    //     Console.Clear();
 
-        double ToAddPrice;
-        while (true)
-        {
-            System.Console.WriteLine("Please input the price of the Film in euros. To go back to the Film manager keep this line empty.");
-            string ToAddPricestring = Console.ReadLine()!;
-            if (ToAddName is null) return;
-            else if (ToAddName == "") return;
-            if (double.TryParse(ToAddPricestring, out _))
-            {
-                ToAddPrice = Convert.ToDouble(ToAddPricestring);
-                break;
-            }
-            else
-            {
-                Console.WriteLine("This is not an integer! Press any button to retry.");
-                Console.ReadKey();
-                Console.Clear();
-            }
-        }
-        Console.Clear();
+    //     double ToAddPrice;
+    //     while (true)
+    //     {
+    //         System.Console.WriteLine("Please input the price of the Film in euros. To go back to the Film manager keep this line empty.");
+    //         string ToAddPricestring = Console.ReadLine()!;
+    //         if (ToAddName is null) return;
+    //         else if (ToAddName == "") return;
+    //         if (double.TryParse(ToAddPricestring, out _))
+    //         {
+    //             ToAddPrice = Convert.ToDouble(ToAddPricestring);
+    //             break;
+    //         }
+    //         else
+    //         {
+    //             Console.WriteLine("This is not an integer! Press any button to retry.");
+    //             Console.ReadKey();
+    //             Console.Clear();
+    //         }
+    //     }
+    //     Console.Clear();
 
-        double toAddFilmRating = 0;
+    //     double toAddFilmRating = 0;
 
-        List<string> toAddGenres = new List<string>();
-        while (true)
-        {
-            System.Console.WriteLine("Do you wish to add any genres? (Y/n) To go back to the Film manager keep this line empty.");
-            string genreAdd = Console.ReadLine()!;
-            if (genreAdd is null) return;
-            if (genreAdd.ToLower() == "y" ^ genreAdd.ToLower() == "yes")
-            {
-                while (true)
-                {
-                    System.Console.WriteLine("Please input the genre to add. To continue leave this line empty");
-                    string genreToAdd = Console.ReadLine()!;
-                    if (genreToAdd is null) break;
-                    else if (genreToAdd == "") break;
-                    else toAddGenres.Add(genreToAdd);
-                    Console.Clear();
-                }
-            }
-            else if (genreAdd.ToLower() == "n" ^ genreAdd.ToLower() == "no")
-            {
-                break;
-            }
-        }
+    //     List<string> toAddGenres = new List<string>();
+    //     while (true)
+    //     {
+    //         System.Console.WriteLine("Do you wish to add any genres? (Y/n) To go back to the Film manager keep this line empty.");
+    //         string genreAdd = Console.ReadLine()!;
+    //         if (genreAdd is null) return;
+    //         if (genreAdd.ToLower() == "y" ^ genreAdd.ToLower() == "yes")
+    //         {
+    //             while (true)
+    //             {
+    //                 System.Console.WriteLine("Please input the genre to add. To continue leave this line empty");
+    //                 string genreToAdd = Console.ReadLine()!;
+    //                 if (genreToAdd is null) break;
+    //                 else if (genreToAdd == "") break;
+    //                 else toAddGenres.Add(genreToAdd);
+    //                 Console.Clear();
+    //             }
+    //         }
+    //         else if (genreAdd.ToLower() == "n" ^ genreAdd.ToLower() == "no")
+    //         {
+    //             break;
+    //         }
+    //     }
 
-        System.Console.WriteLine("Please input the name of the director of the Film. To go back to the Film manager keep this line empty.");
-        string ToAddDirector = Console.ReadLine()!;
-        if (ToAddDirector is null) return;
-        else if (ToAddDirector == "") return;
+    //     System.Console.WriteLine("Please input the name of the director of the Film. To go back to the Film manager keep this line empty.");
+    //     string ToAddDirector = Console.ReadLine()!;
+    //     if (ToAddDirector is null) return;
+    //     else if (ToAddDirector == "") return;
 
-        Film filmToAdd = new Film(ToAddName, ToAddRunTime, ToAddPrice, toAddFilmRating, 0, toAddGenres, ToAddDirector);
+    //     Film filmToAdd = new Film(ToAddName, ToAddRunTime, ToAddPrice, toAddFilmRating, 0, toAddGenres, ToAddDirector);
 
-        FilmSave.AddToJson(filmToAdd);
-        Console.WriteLine("succes! Press any key to continue.");
-        Console.ReadKey();
-    }
+    //     FilmSave.AddToJson(filmToAdd);
+    //     Console.WriteLine("succes! Press any key to continue.");
+    //     Console.ReadKey();
+    // }
 
     public static void ViewFilms()
     {
@@ -247,40 +247,40 @@ static class FilmsManage
         {
             Console.WriteLine("Vul in de film tijd in minuten: Om te stoppen houdt deze lijn leeg");
             string? runtTimeString = Console.ReadLine();
-            if (!string.IsNullOrEmpty(runtTimeString)) return null;
+            if (string.IsNullOrEmpty(runtTimeString)) return null;
             else if (int.TryParse(runtTimeString, out runtime)) break;
         }
 
         double price;
         while (true)
         {
-            Console.Write("vul in de film prijs in euro's: Om te stoppen houdt deze lijn leeg");
+            Console.WriteLine("vul in de film prijs in euro's: Om te stoppen houdt deze lijn leeg");
             string? pricestring = Console.ReadLine();
-            if (!string.IsNullOrEmpty(pricestring)) return null;
+            if (string.IsNullOrEmpty(pricestring)) return null;
             else if (double.TryParse(pricestring, out price) && price > 0) break;
         }
 
         double filmRating;
         while (!double.TryParse(Console.ReadLine(), out filmRating) || filmRating < 0 || filmRating > 10)
         {
-            Console.Write("vul in de rating van de film van 0 tot 10: Om te stoppen houdt deze lijn leeg");            
+            Console.WriteLine("vul in de rating van de film van 0 tot 10: Om te stoppen houdt deze lijn leeg");            
             string? filmratingstring = Console.ReadLine();
-            if (!string.IsNullOrEmpty(filmratingstring)) return null;
+            if (string.IsNullOrEmpty(filmratingstring)) return null;
             else if (double.TryParse(filmratingstring, out filmRating) && filmRating > 0 && filmRating < 10) break;
         }
 
         int releaseYear;
         while (true)
         {
-            Console.Write($"vul het Jaar van uitgave in: Om te stoppen houdt deze lijn leeg");
+            Console.WriteLine($"vul het Jaar van uitgave in: Om te stoppen houdt deze lijn leeg");
             string? releaseYearstring = Console.ReadLine();
-            if (!string.IsNullOrEmpty(releaseYearstring)) return null;
+            if (string.IsNullOrEmpty(releaseYearstring)) return null;
             else if (int.TryParse(releaseYearstring, out releaseYear) && releaseYear > 1800 && releaseYear < DateTime.Now.Year) break;
         }
         string director;
         while (true)
         {
-            Console.Write("vul de regisseur van de film in: Om te stoppen houdt deze lijn leeg");
+            Console.WriteLine("vul de regisseur van de film in: Om te stoppen houdt deze lijn leeg");
             director = Console.ReadLine()!;
             if (string.IsNullOrEmpty(director))
             {
